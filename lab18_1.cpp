8ef38ae39e734ebd21e72e2553f17139ce9cebbf
Delphisss <Kong_led_@hotmail.com>
2023-02-14 14:53:52 +0700

    lab18_1 okay

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B,*C;	
+	int *A,*B,**C; //เพิ่ม **C จาก *C	
 	
 	cout << "Length of A: ";
 	cin >> nA;
 	
-	A = new int;
+	A = new int [nA]; //เพิม A = new int [nA]; จาก A = new int ;
 	cout << "Input Array A: ";
 	for(int i=0; i < nA; i++) cin >> A[i];
 	
 	cout << "Length of B: ";
 	cin >> nB;
 	
-	B = new int;
+	B = new int [nB]; //เพิม A = new int [nB]; จาก A = new int ;
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
-	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
+	C = new int *[nA]; //เพิม A = new int *[nA]; จาก A = new int ;
+	for(int i=0; i < nA; i++) C[i] = new int [nB]; // เพิ่ม [nB]
 	
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i][j]= A[i]*B[j]; // เพิ่ม[i][j] ตรง C
 		}
 	}
 	
@@ -40,8 +40,11 @@ int main(){
 			cout << C[i][j] << "\t";
 		}
 		cout << "\n";
-	}
-     delete A,B,C; 
+	} 
+	delete [] A;   // delete A,B,C; แก้ตรงนี้
+	delete [] B; 
+	for(int i=0; i < nA; i++) delete [] C[i];
+	delete [] C;
 
 	return 0;
 }
