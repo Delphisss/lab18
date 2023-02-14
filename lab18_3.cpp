6cf35834efff1e13163a82e657b3423e46a85de7
Delphisss <Kong_led_@hotmail.com>
2023-02-14 15:43:18 +0700

    lab18_3 okay

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -20,5 +20,25 @@ int main(){
 	
 	return 0;
 }
+//โครงสร้างจากโจทย์เอามาเพิ่มส่วนที่หาย 
+vector<int> randomVector(unsigned int N){	//เพิ่ม vector<int> 
+    vector<int> v; 
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10); //เพิ่ม push_back
+    return v; // เพิ่ม return v เพื่อเก็บค่ากลับไปไว้ใน v
+}
 
+ void showVector(vector<int> v){ //เพิ่ม void กับ vector<int> v
+	cout << "[";
+	for(unsigned int i = 0; i<v.size() ; i++){ // เพิ่มเงื่อนไข i < v.size
+		cout << v[i]; //เพิ่ม v[i]
+		if(i == v.size()-1) cout << "]"; //เพิ่มเงื่อนไข i == v.size()-1
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> A,vector<int> B){ //เพิ่ม int กับ vector<int> A,vector<int> B
+	int sum = 0;
+	for(unsigned int i = 0; i<A.size() ; i++) sum += A[i]*B[i];	//เพิ่มเงื่อนไข i<A.size() กับ sum += A[i]*B[i]
+	return sum; // return ค่าเก็บไว้ใน sum
+}
 //Write Definition of randomVector() showVector() and dotProduct() here
